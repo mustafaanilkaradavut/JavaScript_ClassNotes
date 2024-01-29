faffef11f8b57e27328f71d4de8c0953a208fee2
Mustafa Anil Karadavut <mustafaanilkaradavut@gmail.com>
2024-01-29 08:05:27 +0000

    variables

---
+++ JS-01-Intro-DataTypes-Variables/Js/intro.js
@@ -0,0 +1,16 @@
+//todo                      Important JS Element Using Mehods
+
+console.log("We are in the Intro JS");
+console.error("Error Message");
+console.warn("Warning!");
+
+//? Alert is using that for send a warning.
+// alert("Enter Your Name")
+
+//? document.write is work for the test our codes.
+// document.write("JAVASCRIPT INTRO")
+
+// answer=confirm("Do you want close it?")
+// console.log("answer")
+
+
---
+++ JS-01-Intro-DataTypes-Variables/Js/variables.js
@@ -0,0 +1,41 @@
+x = 15;
+username = "Sacha";
+age = 23;
+
+console.log(username);
+
+// ------------------------------------------------------
+//               JAVASCRIPTS VARIABLES
+// ------------------------------------------------------
+
+//! Const - Let . They become after ES6 . They using for describe the variables.
+
+//? CONST
+//* Const  =  constant  =  sabit
+
+const idnumber = 12673598576;
+// idnumber = 18231273123782137;
+// TypeError: Attempted to assign to readonly property.
+
+//? LET
+
+let total = 0;
+
+x = 23;
+y = 37;
+
+total = x + y;
+console.log(total);
+
+//? GLOBAL AREA : Sharin area that finding all section in program.
+//? LOCAL AREA : It's in the block. It's describe by { } blocks. If - while - for.
+
+const number = 5;
+
+{
+  console.log(number);
+  const pi = 3.14;
+  console.log(pi);
+}
+
+console.log(pi);
