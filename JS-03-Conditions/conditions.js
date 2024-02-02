a49aabd8e092e91c245d06b6860793fbb0635536
Mustafa Anil Karadavut <mustafaanilkaradavut@gmail.com>
2024-02-02 05:54:45 +0000

    conditions

---
+++ JS-03-Conditions/conditions.js
@@ -0,0 +1,14 @@
+/* -------------------------------------------------------------------------- */
+/*                                 CONDITIONS                                 */
+/* -------------------------------------------------------------------------- */
+
+console.log("*** CONDITIONS ***");
+
+// const num1 = prompt("Enter a number");
+//__ We have use second one. Because first one isn't number.
+const num1 = Number(prompt("Enter a number:"))
+const num2 = 5;
+console.log(num1, typeof num1);
+console.log("Toplam:", num1 + num2); //? String Concatination (Birleştirme)
+
+
