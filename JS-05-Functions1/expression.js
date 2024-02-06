ba4ea822b6131187af6470f5067d87ab50da4ecf
Mustafa Anil Karadavut <mustafaanilkaradavut@gmail.com>
2024-02-06 14:23:56 +0000

    expression

--- JS-05-Functions1/expression.js
+++ JS-05-Functions1/expression.js
@@ -4,5 +4,32 @@
 
 console.log("*****  Function Expression  *****");
 
+//?     Example-1
 
+const oddOrEven = function (num) {
+  let result = " ";
 
+  if (num % 2 === 0) {
+    result = `${num} it's Even`;
+  } else {
+    result = `${num} it's Odd`;
+  }
+  return result; //* Return efect like "Break".
+};
+
+const information = oddOrEven(5);
+console.log(information);
+
+console.log(oddOrEven(4));
+console.log(oddOrEven(23));
+
+//?     Example-2
+
+function primeCalculator(base, prime) {
+  return base ** prime;
+}
+
+const prime = primeCalculator(3, 4);
+console.log(prime);
+
+console.log(primeCalculator(5, 3));
