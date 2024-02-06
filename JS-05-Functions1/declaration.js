fde851bb9d0f71facd25db0bd2d67fa0cbdcc107
Mustafa Anil Karadavut <mustafaanilkaradavut@gmail.com>
2024-02-06 13:25:56 +0000

    func declaration

---
+++ JS-05-Functions1/declaration.js
@@ -0,0 +1,17 @@
+/* -------------------------------------------------------------------------- */
+/*                                  FUNCTIONS                                 */
+/* -------------------------------------------------------------------------- */
+
+console.log("***** 1 - FUNCTION DECLARATION *****");
+
+// JS have 3 function snytax : Function Declaration / Function Expression / Arrow Function
+
+//? Example
+
+//* It's not efective for Mordern JS
+//* Describe Function      /        Hoisted (Added up, add memory)
+function hello() {
+  console.log("Hello Cohort16");
+}
+
+hello(); //* Function call / Function invoke
