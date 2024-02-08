2586bf79425cc4c0c2e60f8c1727da5dabade592
Mustafa Anil Karadavut <mustafaanilkaradavut@gmail.com>
2024-02-08 16:12:40 +0000

    arrays update

--- JS-07-Arrays/arrays.js
+++ JS-07-Arrays/arrays.js
@@ -2,4 +2,21 @@
 //!                                  ARRAYS                                   */
 /* -------------------------------------------------------------------------- */
 
+console.log("*****   ARRAYS   *****");
 
+//?         Dizi tanımlama
+/* -------------------------------------------------------------------------- */
+
+//..        Square Bracket  ( Array Literal)
+
+const names = ["Jhon", "Wick", "Robert", true, 6.6, [1, 3], "Break Time"];
+console.log(names);
+console.log("Uzunluk:", names.length);
+console.log(typeof names); //* JS'de her şey object'tir. En temel unsurdur.
+
+//?         Diziden Veri Okuma - Yazma (İndexleme)
+/* -------------------------------------------------------------------------- */
+
+console.log(names[3]);
+console.log(names[1]);
+console.log(names[5][0]);
