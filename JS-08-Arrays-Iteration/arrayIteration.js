b1c4c2832e36c0cfcc2bf6058bd7cb3396e0dcb4
Mustafa Anil Karadavut <mustafaanilkaradavut@gmail.com>
2024-02-12 10:18:22 +0000

    forEach

--- JS-08-Arrays-Iteration/arrayIteration.js
+++ JS-08-Arrays-Iteration/arrayIteration.js
@@ -5,13 +5,24 @@
 console.log("*****  ITERATION  *****");
 
 const numbers = [4, 5, 1, 4, 6, 9];
-numbers.forEach((num) => console.log(num));
+numbers.forEach((x) => console.log(x));
 
+
+
+
+
+//?             Example 2        :           2 Parametreli forEach 
+numbers.forEach((num, i) => console.log("Value:", num, "Alignment:", i));
 console.log("************");
-
-//?             Alternative Way
+//?             Alternative Way 
 
 numbers.forEach(print);
 function print(x) {
   console.log(x);
 }
+
+
+//?             Example 3        :           Toplam 
+let sum = 0;
+numbers.forEach((number)=> (sum+=number))
+console.log("SUM: ", sum);
