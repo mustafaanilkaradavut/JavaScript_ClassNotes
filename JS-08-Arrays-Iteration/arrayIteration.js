7eec91cc384b9ca24274a7fdad35f4778fcf624b
Mustafa Anil Karadavut <mustafaanilkaradavut@gmail.com>
2024-02-12 10:35:05 +0000

    forEach completed

--- JS-08-Arrays-Iteration/arrayIteration.js
+++ JS-08-Arrays-Iteration/arrayIteration.js
@@ -7,22 +7,35 @@ console.log("*****  ITERATION  *****");
 const numbers = [4, 5, 1, 4, 6, 9];
 numbers.forEach((x) => console.log(x));
 
-
-
-
-
-//?             Example 2        :           2 Parametreli forEach 
+//?             Example 1        :           2 Parametreli forEach
 numbers.forEach((num, i) => console.log("Value:", num, "Alignment:", i));
 console.log("************");
-//?             Alternative Way 
+
+//?             Alternative Way
 
 numbers.forEach(print);
 function print(x) {
   console.log(x);
 }
 
-
-//?             Example 3        :           Toplam 
+//?             Example 2        :           Toplam
 let sum = 0;
-numbers.forEach((number)=> (sum+=number))
+numbers.forEach((number) => (sum += number));
 console.log("SUM: ", sum);
+
+//?             Example 3        :           3 Parametreli forEach
+
+const examResult = [40, 50, 10, 30, 60, 20, 70];
+
+//!         Süslü parantezli olarak da yazılabilir.
+// examResult.forEach((result,indis,array) =>{
+//     array[indis]=result*1.1
+// })
+
+examResult.forEach((result, indis, array) => (array[indis] = result * 1.1));
+console.log(examResult);
+
+//__ forEach doesn't return affect. ( Void Function )
+console.log(examResult.forEach((x) => x * x)); //*         undefined
+const pratice = examResult.forEach((x) => x * x);
+console.log(pratice); //*           undefined
