e072e4de2eb6de1a81df15be3453d4590090dee0
Mustafa Anil Karadavut <mustafaanilkaradavut@gmail.com>
2024-04-02 03:02:34 +0100

    innerHTML

--- JS-12-createElement-events/createNode.js
+++ JS-12-createElement-events/createNode.js
@@ -14,7 +14,7 @@ document.querySelector("ul").appendChild(newLi);
 
 /* -------------------------------------------------------------------------- */
 
-//.. 2. bir "li" oluşturalım. 
+//.. 2. bir "li" oluşturalım.
 const newLi2 = document.createElement("li");
 newLi2.textContent = "C++";
 
@@ -22,9 +22,12 @@ newLi2.textContent = "C++";
 const react = document.querySelector("ul li:nth-child(4)");
 react.before(newLi2, newLi);
 
-//* ------- ID, CLASS GIBI OZELLIKLERI ATAMA ------
+/* -------------------------------------------------------------------------- */
+//..                    ID, CLASS GIBI OZELLIKLERI ATAMA                      */
+/* -------------------------------------------------------------------------- */
 
-//? 1- element.attribute = "yeni deger"
+//__ 1 - element.attribute = "yeni değer"
+
 //* yazma
 newLi.className = "list bold text-success bg-warning";
 newLi.id = "new-li";
@@ -33,23 +36,31 @@ newLi.name = "list";
 //* okuma
 console.log(newLi.className);
 
-//? 2- setAttribute(atr,"deger") getAttrbute("atr")
+//__ 2 - setAttribute(atr,"değer") getAttrbute("atr")
+
 newLi2.setAttribute("class", "list red bold");
 
 console.log(newLi2.getAttribute("id"));
 console.log(newLi2.getAttribute("class"));
 
-//? 3) Yontem (classList)
-newLi.classList.add("border", "border-danger", "border-3");
+//! Eğer aşğı tarafa newLi.className adında farklı bir veri tanımlarsak, tanımladığımız veriler öncekinin üstüne yazar.
+//! Bunun için classList özelliği kullanarak önceki verilerin ezilmesini engelleriz.
+
+//__ 3 - Yöntem (classList)
+newLi.classList.add("border", "border-danger", "border-3"); //* Üzerine ekleme yapar. Eski veri korunur.
 
 newLi.classList.remove("bold", "text-success");
 
-console.log(newLi2.classList.contains("red"));
+console.log(newLi2.classList.contains("red")); //* içerisindeki class'ın olup olmadığını bize gösterir.
 
-newLi.classList.toggle("red");
-newLi2.classList.toggle("red");
+newLi.classList.toggle("red"); //* Bu attribute ile eğer red class'ı yoksa ekler, var ise siler.
+newLi2.classList.toggle("red"); //! Aç kapa mantığının olduğu (button gibi) yerlerde toggle kullanırız.
 
-//* ------- YENI BIR ELEMENT OLUSTURMA (InnerHTML)------
+/* -------------------------------------------------------------------------- */
+//..                 YENI BIR ELEMENT OLUSTURMA (InnerHTML)                   */
+/* -------------------------------------------------------------------------- */
+
+//__ Eğer kodu ezmek istiyorsak = işareti kullanırız, eğer ekleme yapmak istiyorsak += işareti kullanmalıyız. Back Tick kullanmalıyız. (option + ",")
 
 document.querySelector("main").innerHTML += `
     <div class="d-flex gap-2">
