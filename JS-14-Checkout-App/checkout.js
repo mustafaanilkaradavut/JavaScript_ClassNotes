c1ca33291987eaa713bb0efcd3229cff93c94b3f
Mustafa Anil Karadavut <mustafaanilkaradavut@gmail.com>
2024-04-05 02:55:17 +0100

    traversing dom

--- JS-14-Checkout-App/checkout.js
+++ JS-14-Checkout-App/checkout.js
@@ -30,22 +30,23 @@ const FREE_SHIPPING_LIMIT = 3000;
 const SHIPPING_PRICE = 25.99;
 const TAX_RATE = 0.18;
 
-//? Selectors
+//.. Selectors
+
 const deleteAllBtn = document.querySelector(".delete-div .fa-trash-can");
 const products = document.querySelector("article.products");
 
-//? EVENT HANDLERS
+//.. EVENT HANDLERS
 
 deleteAllBtn.addEventListener("click", (e) => {
   products.textContent = "No product";
   products.classList.add("no-product"); // classList.add kısmında  eklenmez.
-  document.querySelector(".delete-div").remove();
+  document.querySelector(".delete-div").remove(); //__ Delete Product kısmını eğer bir ürünümüz kalmazsa kaldırmaz için bu kodu yazarız.
   // document.querySelector(".delete-div").style.display = "none"
   calculateTotalPrice();
 });
 
 products.addEventListener("click", (e) => {
-  // if (e.target.classList.contains("fa-plus")) {
+  // if (e.target.classList.contains("fa-plus")) {  //* Class aramalarında (.) veya (#) kullanmayız.
   //   alert("plus")
   // } else if (e.target.classList.contains("fa-minus")) {
   //   alert("minus")
@@ -54,15 +55,19 @@ products.addEventListener("click", (e) => {
   // }
 
   if (e.target.classList.contains("fa-plus")) {
-    //? static olarak quantity kullanirsak hangi + butonuna tiklanilirsa tiklanilsin akistaki ilk urunun degeri artar.
+    //__ Static olarak quantify kullanırsak hangi + butonuna tıklanırsa tıklansın akıştaki ilk ürünün değeri artar. Bu yüzden Traversing DOM kullanmamız lazım.
 
-    //? Traversing DOM
+    //.. Traversing DOM
+
     e.target.previousElementSibling.textContent++;
+    //__ Yukarıdaki code mantığına göre eventları ayırırız. (fa-pluslar) Ardından event target ile yerimizi belirleriz. Yerimizi belirledikten sonra ise buna göre sibling kullanırız. Böylelikle seçtiğimiz target'in önündeki yeri arttırırız.
     // document.getElementById("quantity").textContent++
 
     // const discountedPrice = document.getElementById("discounted-price")
     // console.log(discountedPrice.innerText)
-
+    
+    //?     Adet sayısı değiştirtikten sonra değişen fiyatı hesaplayacak bir event kurarız.
+    
     calculatePrice(e.target);
   } else if (e.target.classList.contains("fa-minus")) {
     if (e.target.nextElementSibling.textContent > 1) {
