d3bf3dcdb57c803e861214b654640cbe667f8da9
Mustafa Anil Karadavut <mustafaanilkaradavut@gmail.com>
2024-04-11 07:30:49 +0100

    main

--- JS-17-ASYNC2/async-await.js
+++ JS-17-ASYNC2/async-await.js
@@ -17,44 +17,47 @@ const getNews = async () => {
 
   const BASE_URL = `https://newsapi.org/v2/`;
   const API_KEY = `f33b836eabbd49888b8ef386ed77c55f`;
-  const queryString = `top-headlines?country=tr&category=technology`; 
-  const URL = `${BASE_URL}${queryString}&apiKey=${API_KEY}`;
-
-  try {
-    const res = await fetch(URL);
-
-    //? Error handing
-    if (!res.ok) {
-      throw new Error(`${res.status}`);
-    }
-    const data = await res.json();
-    dipslayNews(data.articles);
-  } catch (error) {
-    const newsArticle = document.getElementById("news");
-    newsArticle.innerHTML = `
-          <img src="./img/404.png" alt="" />
-      `;
-  }
-};
-
-const dipslayNews = (news) => {
-  const newsArticle = document.getElementById("news");
-  const defaultImage = "./img/404.png";
-
-  news.forEach(({ urlToImage, url, content, title }) => {
-    newsArticle.innerHTML += `
-          <div class="card col-sm-6 col-md-4 col-lg-3">
-              <img src="${
-                urlToImage || defaultImage
-              }" class="card-img-top" alt="...">
-              <div class="card-body">
-                  <h5 class="card-title">${title}</h5>
-                  <p class="card-text">${content || "No Content"}</p>
-                  <a href="${url}" target="_blank" class="btn btn-primary">Detail</a>
-              </div>
-          </div>
-      `;
-  });
+  const queryString = `top-headlines?country=tr&category=technology`;
+  const URL = `${BASE_URL}${queryString}&apiKey=${API_KEY}`; //__ Bunu yapmak zorunlu değildir. Parça parça yazmamız bize avantaj getirir.
+  console.log(URL);   //__ Doğru bir link geliyor mu onu test ederiz.
 };
 
 getNews();
+//   try {
+//     const res = await fetch(URL);
+
+//     //? Error handing
+//     if (!res.ok) {
+//       throw new Error(`${res.status}`);
+//     }
+//     const data = await res.json();
+//     dipslayNews(data.articles);
+//   } catch (error) {
+//     const newsArticle = document.getElementById("news");
+//     newsArticle.innerHTML = `
+//           <img src="./img/404.png" alt="" />
+//       `;
+//   }
+// };
+
+// const dipslayNews = (news) => {
+//   const newsArticle = document.getElementById("news");
+//   const defaultImage = "./img/404.png";
+
+//   news.forEach(({ urlToImage, url, content, title }) => {
+//     newsArticle.innerHTML += `
+//           <div class="card col-sm-6 col-md-4 col-lg-3">
+//               <img src="${
+//                 urlToImage || defaultImage
+//               }" class="card-img-top" alt="...">
+//               <div class="card-body">
+//                   <h5 class="card-title">${title}</h5>
+//                   <p class="card-text">${content || "No Content"}</p>
+//                   <a href="${url}" target="_blank" class="btn btn-primary">Detail</a>
+//               </div>
+//           </div>
+//       `;
+//   });
+// };
+
+// getNews();
