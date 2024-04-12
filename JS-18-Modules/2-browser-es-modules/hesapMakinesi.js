7aea7a10e18c1d70393fbef7debc3d9849dcfae6
Mustafa Anil Karadavut <mustafaanilkaradavut@gmail.com>
2024-04-12 04:53:47 +0100

    module import

--- JS-18-Modules/2-browser-es-modules/hesapMakinesi.js
+++ JS-18-Modules/2-browser-es-modules/hesapMakinesi.js
@@ -2,17 +2,26 @@
 //..                            hesapmakinesi.JS                              */
 /* -------------------------------------------------------------------------- */
 
+//?             dışarıya açılıyor.  Export oluyor.
+//?             named export
+// export const add = (a, b) => a + b
+// export const sub = (a, b) => a - b
+// export const mul = (a, b) => a * b
+// export const div = (a, b) => a / b
+
 const add = (a, b) => a + b;
 const sup = (a, b) => a - b;
 const mul = (a, b) => a * b;
 const div = (a, b) => a / b;
 
-const PI = 3.14;
+//? named export (toplu şekilde export etme yöntemidir.)
+export { add, sub, div, mul };
+
+export const PI = 3.14;
 
 const primeNumbers = [2, 3, 5, 7, 13];
 
-//?         default export (sadece bir kere kullanılır.)
-
+//? default export (sadece bir kere kullanılır.)
 const message = () => {
   console.log("End of Module");
 };
