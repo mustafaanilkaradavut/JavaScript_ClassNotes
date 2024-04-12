ba0d30d59759524fabaf856746ee4c7024e02734
Mustafa Anil Karadavut <mustafaanilkaradavut@gmail.com>
2024-04-12 04:29:54 +0100

    comment

---
+++ JS-18-Modules/1-without-modules/calculate.js
@@ -0,0 +1,15 @@
+// "use strict" //! Derleyici direktifi, hatalar konusunda katı olup bize kod yazma aşamasında sıkça error verip hataları gösterir.
+
+console.log("CALCULATE.JS");
+
+const totalPrice = (products) => {
+  return products.reduce((sum, product) => sum + product.price, 0);
+};
+
+//? Global namespace
+// const students = ["Canan", "Can"]
+
+myVariable = "my string"; //! use Strict -> myVariable is not defined
+console.log(myVariable);
+
+console.log(this); //? window
