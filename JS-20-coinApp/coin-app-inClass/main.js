4866cf583831ee4d5f91d98be6f2b123a1a530b7
Mustafa Anil Karadavut <mustafaanilkaradavut@gmail.com>
2024-04-15 10:30:22 +0100

    npm node-sass

---
+++ JS-20-coinApp/coin-app-inClass/main.js
@@ -0,0 +1,7 @@
+import "./scss/style.scss";
+//__ Bu importu yaptığımızda bize hata verir ve sass yüklenip yüklenmediğini sorar.
+//__ Bunun için npmjs'den sass'ın third party kütüphanesini indiririz.
+//__ npm i sass yaparsak kulurum yaparız. Ardından npm run dev ile kurulumu tamamlarız.
+
+
+
