99204453541b041b116e2507a54e59edce7bc27c
Mustafa Anil Karadavut <mustafaanilkaradavut@gmail.com>
2024-07-20 07:01:09 +0100

    spec

---
+++ Cypress-App/recipe-app-master/cypress/e2e/spec.cy.js
@@ -0,0 +1,11 @@
+describe('template spec', () => {
+  it('passes', () => {
+    cy.visit('https://example.cypress.io')
+  })
+})
+
+describe('template spec', () => {
+  it('passes2', () => {
+    cy.visit('https://examle.cypress.io')
+  })
+})
